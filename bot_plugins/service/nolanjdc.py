0fb1a2aa30217d9b69effe2fd5450f644c2f142f
U188 <54225629+U188@users.noreply.github.com>
2021-11-06 20:46:00 +0800

    Add files via upload

---
+++ bot_plugins/service/nolanjdc.py
@@ -0,0 +1,30 @@
+import requests
+uu='nolanjdc地址' #jdc地址如http://asadasd:5701
+def sendsms(phone):
+    url=f'{uu}/api/SendSMS'
+    data={
+        'Phone':phone,
+        'qlkey':0
+    }
+    result=requests.post(url,json=data)
+    return result.json()['message']
+    
+def AutoCaptcha(phone):
+    url=f'{uu}/api/AutoCaptcha'
+    data={
+        'Phone':phone
+    }
+    result=requests.post(url,json=data,timeout=240)
+    return result.json()['success']
+
+def VerifyCode(phone,qq,code):
+    url=f'{uu}/api/VerifyCode'
+    qq=str(qq)
+    data={
+        'Phone':phone,
+        'QQ':qq,
+        'qlkey':0,
+        'Code':code
+    }
+    result=requests.post(url,json=data)
+    return result.json()['message']
