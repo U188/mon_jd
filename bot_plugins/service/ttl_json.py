18aa9a476ad4117ecc490b4922b2c95d66566da6
U188 <54225629+U188@users.noreply.github.com>
2021-11-20 18:00:37 +0800

    Add files via upload

---
+++ bot_plugins/service/ttl_json.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+## 账号管理
+accounts = [
+    {
+        'iphone':'',
+        'passw':'',
+        'comp':''
+    },
+    {
+        'iphone': '',
+        'passw': '',
+        'comp': ''
+    },
+]
