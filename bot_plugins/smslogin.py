c9be2fead6523172888eb5097c6317e3beffccb5
U188 <54225629+U188@users.noreply.github.com>
2021-11-20 18:49:17 +0800

    Update smslogin.py

--- bot_plugins/smslogin.py
+++ bot_plugins/smslogin.py
@@ -17,12 +17,13 @@ async def ssss(session: CommandSession):
 	#else:
 	ipone = session.current_arg_text.strip()
 	if not ipone:
+		qqnum=str(session.ctx['user_id'])
 		await session.send('请在5分钟内结束这次对话！')
 		time.sleep(1)
 		ipone = await session.aget(prompt='嗨！ 请输入手机号码:')
-		phone = re.compile('^(13(0|1|2|3|7|8|9|6|5|4)|16(0|1|2|3|7|8|9|6|5|4)|17(0|8|5|6|2|3|7)|18(0|1|2|3|4|5|6|7|8|9)|15(1|2|3|5|6|7|8|9)|19(0|1|3|8|9))\d{8}$')
+		phone = re.compile('^(13(0|1|2|3|7|8|9|6|5|4)|16(0|1|2|3|7|8|9|6|5|4)|17(0|8|5|6|2|3|7)|18(0|1|2|3|4|5|6|7|8|9)|15(0|1|2|3|5|6|7|8|9)|19(0|1|3|8|9))\d{8}$')
 		if re.match(phone, ipone):
-				await session.send('请等待。。。。')
+				await session.send('准备滑块验证中。。。')
 				send_code = nolanjdc.sendsms(ipone)
 				time.sleep(8)
 				if '安全验证' in send_code:
@@ -33,24 +34,24 @@ async def ssss(session: CommandSession):
 						succ=nolanjdc.AutoCaptcha(ipone)
 
 						time.sleep(3)
-						await session.send(f'{succ}')
+						#await session.send(f'{succ}')
 						if succ != True:
 							time.sleep(1)
-							if n==3:
+							if n==5:
 								await session.send(f'第{n}次滑块验证失败,退出程序。去{url}登陆吧！')
 								break
-							await session.send(f'第{n}次滑块验证失败，进入下一次验证！')
+							await session.send(f'验证失败，进入下一次验证！')
 							n += 1
 							continue
 						else:
-							code = await session.aget(prompt='嗯！验证码该发我了:')
+							code = await session.aget(prompt='验证码该发我了:')
 							if len(code) != 6:
 								await session.send('丢！你数数你验证码多少位！再见')
 								break
 							else:
 								if code:
-									qq = await session.aget(prompt='嗯！QQ该发我了:')
-									msg1 = nolanjdc.VerifyCode(ipone,qq,code)
+									#qq = await session.aget(prompt='嗯！QQ该发我了:')
+									msg1 = nolanjdc.VerifyCode(ipone,qqnum,code)
 									if '成功' in msg1:
 										await session.send('恭喜上车成功!')
 									else:
