06912b94ce1b097fc73c1f39d5ee18a954310260
U188 <54225629+U188@users.noreply.github.com>
2021-11-20 20:58:50 +0800

    Update ttl_json.py

--- bot_plugins/service/ttl_json.py
+++ bot_plugins/service/ttl_json.py
@@ -1,15 +1,1 @@
-# !/usr/bin/env python3
-# -*- coding: utf-8 -*-
-## 账号管理
-accounts = [
-    {
-        'iphone':'',
-        'passw':'',
-        'comp':''
-    },
-    {
-        'iphone': '',
-        'passw': '',
-        'comp': ''
-    },
-]
+accounts = [{'qq': '', 'iphone': '', 'passw': '', 'comp': ''}, {'qq': '', 'iphone': '', 'passw': '', 'comp': ''}]
