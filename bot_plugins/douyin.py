3b588bd01264a4b9eb7344d2e5e94cd339617a7f
U188 <54225629+U188@users.noreply.github.com>
2021-11-28 15:03:02 +0800

    Create douyin.py

---
+++ bot_plugins/douyin.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import requests,time,re
+import os,sys
+from nonebot.command import CommandSession
+from nonebot.experimental.plugin import on_command
+import time, re
+path=os.path.join(os.path.dirname(__file__))
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
+    }
+def get_url():
+    url='http://api.qemao.com/api/douyin/'
+    s= requests.get(url,headers=headers,allow_redirects=True)
+    return s.url
+def dowm_load(url,name):
+    movie_url = url
+    movie_name = name
+    downsize = 0
+    #print('开始下载')
+    #startTime = time.time()
+    req = requests.get(movie_url, headers=headers, stream=True, verify=False)
+    with(open(path+'/vido/'+movie_name + '.mp4', 'wb')) as f:
+        for chunk in req.iter_content(chunk_size=10000):
+            if chunk:
+                f.write(chunk)
+                downsize += len(chunk)
+                #line = 'downloading %d KB/s - %.2f MB， 共 %.2f MB'
+                #line = line%(downsize/1024/(time.time()-startTime),downsize/1024/1024,downsize/1024/1024)
+                #print(line)
+
+def move_vido(name):
+    os.remove(name)
+    #print(f'已移除{name}')
+
+@on_command('扭一扭', aliases=('抖音', '小姐姐'))
+async def ssss(session: CommandSession):
+    url=get_url()
+    name=re.findall(r'(?<=\/lib\/).+(?=.mp4)',url)[0]
+    dowm_load(url,name)
+    time.sleep(10)
+    vido_path=f'{path}/vido/{name}.mp4'
+    await session.send('[CQ:video,file=file:///{}]'.format(vido_path))
+    time.sleep(5)
+    move_vido(vido_path)
