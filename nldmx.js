9739a8936e32857f2b865057f108942e2b51fde6
U188 <54225629+U188@users.noreply.github.com>
2024-02-03 21:20:56 +0800

    更新 nldmx.js

---
+++ nldmx.js
@@ -0,0 +1,18 @@
+/*********************************
+解锁部分功能
+**************************************
+
+
+[rewrite_local]
+https://api-jiaoyu-brain.vas.lutongnet.com:8887/nldmx-mb-api/nldmx/user/get-free-trial-info url script-response-body https://raw.githubusercontent.com/u188/mon_jd/main/nldmx.js
+
+[mitm]
+hostname = api-jiaoyu-brain.vas.lutongnet.com:8887
+
+*************************************/
+
+
+var obj = JSON.parse($response.body);
+obj.data.freeTrialFlag ="false";
+
+$done({ body: JSON.stringify(obj) });
